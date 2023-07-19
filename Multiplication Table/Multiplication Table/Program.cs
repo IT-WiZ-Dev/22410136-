83476886f10f89d586faff84b34e9b01bac2a80b
鈴木陽翔 <131630585+Haru62129@users.noreply.github.com>
2023-07-19 13:16:37 +0900

    Add files via upload

---
+++ Multiplication Table/Multiplication Table/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   * * * 九九の表 * * *");
+            Console.Write("   |");
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.Write($"{i,3}");
+            }
+            Console.WriteLine("\n--------------------------------");
+
+            for (int x = 1; x <= 9; x++)
+            {
+                Console.Write($"{x}  |");
+                for (int y = 1; y <= 9; y++)
+                {
+                    Console.Write($"{x * y,3}");
+                }
+                Console.Write('\n');
+            }
+
+            Console.WriteLine("\n終了するには何かキーを押してください。");
+            Console.Read();
+        }
+    }
+}
