d0d8b1fb5fe7de09f74d231125e573b68bed0c5c
鈴木陽翔 <131630585+Haru62129@users.noreply.github.com>
2023-07-19 13:16:00 +0900

    Add files via upload

---
+++ Gues/Gues/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int Answer = 32;
+            int low = 0, high = 100;
+            int n, count = 1;
+
+            do
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Console.Write($"{low}から{high}の間の数値を当ててください。＞ ");
+                        n = int.Parse(Console.ReadLine());
+                    }
+                    catch (FormatException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        goto END;
+                    }
+                    if (n >= low && n <= high)
+                        break;
+                    else
+                        Console.WriteLine("入力値が正しくありません。再入力してください。");
+                }
+                if (n != Answer)
+                {
+                    count++;
+                    if (n < Answer)
+                    {
+                        Console.WriteLine("答はもっと大きいです。\n");
+                        low = n + 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("答はもっと小さいです。\n");
+                        high = n - 1;
+                    }
+                }
+            } while (n != Answer);
+            Console.WriteLine($"おめでとう。{count}回目で当たりました。");
+
+        END:
+            Console.WriteLine("\n終了するには何かキーを押してください。");
+            Console.Read();
+        }
+    }
+}
